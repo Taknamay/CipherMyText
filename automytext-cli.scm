7178c4dff564d9ce95bcec204f1d1c937c3b3f02
Jason K. MacDuffie <taknamay@gmail.com>
2015-07-13 10:45:07 -0400

    implement the cipher in the main program

--- automytext-cli.scm
+++ automytext-cli.scm
@@ -3,7 +3,7 @@
         (scheme load)
         (scheme write)
         (scheme process-context)
-	(srfi 1))
+        (srfi 1))
 
 (load "./autokey.scm")
 
@@ -20,41 +20,61 @@ Options:
 
 (define (extract-iterations args)
   (let loop ((list-out '())
-	     (list-in args))
+             (list-in args))
     (if (null? list-in)
-	(values args 1)
-	(let ((value (car list-in)))
-	  (if (and (char=? (string-ref value 0) #\-)
-		   (char=? (string-ref value 1) #\i))
-	      (if (not (char=? (string-ref value 2) #\=))
-		  (usage)
-		  (values (append list-out
-				  (cdr list-in))
-			  (string->number (substring value 3 (string-length value)))))
-	      (loop (cons value list-out) (cdr list-in)))))))
+        (values args 1)
+        (let ((value (car list-in)))
+          (if (and (char=? (string-ref value 0) #\-)
+                   (char=? (string-ref value 1) #\i))
+              (if (not (char=? (string-ref value 2) #\=))
+                  (usage)
+                  (values (append list-out
+                                  (cdr list-in))
+                          (string->number (substring value 3 (string-length value)))))
+              (loop (cons value list-out) (cdr list-in)))))))
 
 (define (extract-mode args)
   (if (member "-d" args)
       (values (delete "-d" args)
-	      'decipher)
+              'decipher)
       (values (if (member "-e" args)
-		  (delete "-e" args)
-		  args)
-	      'encipher)))
+                  (delete "-e" args)
+                  args)
+              'encipher)))
 
 (define (extract-punct args)
   (if (member "-p" args)
       (values (delete "-p" args)
-	      #t)
+              #t)
       (values args #f)))
 
 (define (check-undetected args)
   (for-each (lambda (x)
-	      (if (char=? (string-ref x 0) #\-)
-		  (usage)))
-	    args)
+              (if (char=? (string-ref x 0) #\-)
+                  (usage)))
+            args)
   (if (not (= (length args) 2))
       (usage)))
+
+(define (xcipher text key mode iters punct)
+  (let loop ((i 0)
+             (current-text text))
+    (if (< i iters)
+        (let ((a (if (eq? mode 'encipher)
+                     (encipher current-text key)
+                     (decipher current-text key))))
+          (loop (+ i 1) a))
+        (if punct
+            (display (restore-punctuation text current-text))
+            (let prnloop ((j 0)
+                          (l (string->list current-text)))
+              (unless (null? l)
+                      (display (car l))
+                      (if (and (= (modulo j 5) 0)
+                               (not (= j 0)))
+                          (display " "))
+                      (prnloop (+ j 1) (cdr l)))))))
+  (newline))
 
 (define (main-prog args)
   (define-values (arg-iter iters)
@@ -71,30 +91,31 @@ Options:
   (if (or (member "--version" new-args)
           (member "-v" new-args))
       (version))
-  (check-undetected new-args))
+  (check-undetected new-args)
+  (xcipher (car new-args) (cadr new-args) cipher-mode iters punctuate))
 
 (main-prog (cdr (command-line)))
 
 #|
 
-    text = args[0]
-    key = args[1]
+text = args[0]
+key = args[1]
 
-    for i in range(iterate):
-        a = ''
-        if (mode == 0):
-            a = encipher(text, key)
-        else:
-            a = decipher(text, key)
-        text = a
-    if punctuate:
-        a = restore_punctuation(args[0], a)
-        print(a)
-    else:
-        for i in range(len(a)):
-            if (i % 5 == 0 and i != 0):
-                print(' ', end='')
-            print(a[i], end='')
-        print()
+for i in range(iterate):
+a = ''
+if (mode == 0):
+a = encipher(text, key)
+else:
+a = decipher(text, key)
+text = a
+if punctuate:
+a = restore_punctuation(args[0], a)
+print(a)
+else:
+for i in range(len(a)):
+if (i % 5 == 0 and i != 0):
+print(' ', end='')
+print(a[i], end='')
+print()
 
 |#
