1052b431e7e14e4fe2aaea0f5aed8cf2f5b6f1a0
Jason K. MacDuffie <taknamay@gmail.com>
2016-04-21 19:34:57 -0400

    Add repeating key cipher

---
+++ reptkey.scm
@@ -0,0 +1,32 @@
+
+;;;
+;;; Repeating key encipher and decipher
+;;;
+;;; Copyright 2016 Jason K. MacDuffie
+;;; License: GPLv3+
+;;;
+
+(import (scheme base)
+        (scheme load)
+        (srfi 1))
+
+(load "./cipher-ops.scm")
+
+(define (reptkey-xcipher pt-in key-in direction)
+  ;; A repeating key cipher, commonly known as "Vigenere"
+  ;; I reject that name because it has little to do with
+  ;; Vigenere at all. Anyway, this just involves taking
+  ;; a key and repeating it until the end of the plain
+  ;; text, then applying tabula recta.
+  ;;
+  ;; Encipherment and decipherment are similar, so I
+  ;; am sharing the code body for both.
+  (define l (apply circular-list key-in))
+  (direction pt-in l))
+
+(define (reptkey-encipher pt-in key-in)
+  (reptkey-xcipher pt-in key-in runkey-encipher))
+
+(define (reptkey-decipher pt-in key-in)
+  (reptkey-xcipher pt-in key-in runkey-decipher))
+
