b48f9f805dea71ed2d865b8a391b92671e03681b
Jason K. MacDuffie <taknamay@gmail.com>
2016-05-03 15:26:57 -0400

    Move monoalphabetic into its own file

--- cipher-ops.scm
+++ cipher-ops.scm
@@ -105,30 +105,6 @@
             (display " ")
             (loop l 0))))))))
 
-(define (mono-encipher pt-in key-in)
-  ;; Encipher pt-in with a monoalphabetic cipher, where
-  ;; key-in is a list of the letters with which to replace
-  ;; the plaintext letters.
-  (define vector-key (list->vector key-in))
-  (map (lambda (c) (vector-ref vector-key
-                               (letter->integer c)))
-       pt-in))
-
-(define (mono-decipher ct-in key-in)
-  ;; Inverse of mono-encipher
-  (define vector-key (make-vector 26))
-  (let loop ((i 0)
-             (in key-in))
-    ;; We need to build the inverse key
-    (when (< i 26)
-      (vector-set! vector-key
-                   (letter->integer (car in))
-                   (integer->letter i))
-      (loop (+ i 1) (cdr in))))
-  (map (lambda (c) (vector-ref vector-key
-                               (letter->integer c)))
-       ct-in))
-
 (define (runkey-encipher pt-in key-in)
   ;; Simplest polyalphabetic cipher. Potentially useful
   ;; as the base of other polyalphabetic ciphers.
---
+++ monokey.scm
@@ -0,0 +1,37 @@
+
+;;;
+;;; Monoalphabetic encipher and decipher
+;;;
+;;; Copyright 2016 Jason K. MacDuffie
+;;; License: GPLv3+
+;;;
+
+(import (scheme base)
+        (scheme load))
+
+(load "./cipher-ops.scm")
+
+(define (mono-encipher pt-in key-in)
+  ;; Encipher pt-in with a monoalphabetic cipher, where
+  ;; key-in is a list of the letters with which to replace
+  ;; the plaintext letters.
+  (define vector-key (list->vector key-in))
+  (map (lambda (c) (vector-ref vector-key
+                               (letter->integer c)))
+       pt-in))
+
+(define (mono-decipher ct-in key-in)
+  ;; Inverse of mono-encipher
+  (define vector-key (make-vector 26))
+  (let loop ((i 0)
+             (in key-in))
+    ;; We need to build the inverse key
+    (when (< i 26)
+      (vector-set! vector-key
+                   (letter->integer (car in))
+                   (integer->letter i))
+      (loop (+ i 1) (cdr in))))
+  (map (lambda (c) (vector-ref vector-key
+                               (letter->integer c)))
+       ct-in))
+
