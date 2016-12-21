87aa711d65cef21d19baa86c6d7cfdb988013978
Jason K. MacDuffie <taknamay@gmail.com>
2016-12-20 22:31:02 -0500

    try to start working on the layout of cmt

--- cmt.scm
+++ cmt.scm
@@ -8,8 +8,55 @@
   (scheme write)
   (macduffie queue)
   (macduffie cipher)
-  (libui))
+  (prefix (libui) ui:))
 
-(display apply-cipher)
-(newline)
+;; Create an association list of ciphers
+(define cipher-list
+  `(("Autokey" ,autokey-encipher ,autokey-decipher)
+    ("Reptkey (Vigenere)" ,reptkey-encipher ,reptkey-decipher)
+    ("Monoalphabetic" ,mono-encipher ,mono-decipher)
+    ("Caesar" ,caesar-encipher ,caesar-decipher)
+    ("ROT13" ,rot13 ,rot13)))
+
+;; use a grid
+;; Input: [       ]
+;; Key:   [       ]
+;; Radio  choicebox
+;; en/de  Go! btn
+;; Output:[       ]
+
+(define (btn-press bt)
+  (ui:entry-text-set!
+   (ui:widget-by-id 'greeting)
+   (string-append "Hello, "
+                  (ui:entry-text (ui:widget-by-id 'name))
+                  "!")))
+
+(ui:init!)
+
+(ui:widgets
+ `(window
+   (@ (id main)
+      (title "Hello")
+      (width 200)
+      (height 100)
+      (closing ,(lambda (wi) (ui:quit!))))
+   (vbox
+    (label
+     (@ (text "Enter your name:")))
+    (entry
+     (@ (id name)
+        (text "")
+        (read-only? #f)))
+    (button
+     (@ (id btn)
+        (text "Greet")
+        (clicked ,btn-press)))
+    (entry
+     (@ (id greeting)
+        (text "")
+        (read-only? #t))))))
+
+(ui:control-show! (ui:->control (ui:widget-by-id 'main)))
+(ui:main)
 
